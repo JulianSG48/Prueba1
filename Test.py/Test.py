bb2aaf4487643c984fae67b761c57c5fa17e527c
JulianS <jsantanag@unal.edu.co>
2024-11-25 15:50:25 -0500

    PruebaJJ

--- Test.py/Test.py
+++ Test.py/Test.py
@@ -1,4 +1,19 @@
-i = 0
+""" i = 0
 while(i <= 10):
     print("Este es el número", i)
-    i = i + 1
+    i = i """
+  
+###################################################
+
+print("Digite un número entero entre 1 y 255")
+def valores_ascii():
+    while True:
+        valor = int(input("Digite el número ")) 
+    
+    return(valor)
+
+x = valores_ascii() 
+
+print(f"El valor {x} corresponde a la letra mayúscula en")
+
+
